d591b936dd0525e266fad393e7fa56733c196cb7
Alex K <damned182@mail.ru>
2020-05-03 17:43:40 +0300

    add modules to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,18 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
+    JwtModule.register({
+      secret: 'supersecret',
+      signOptions: {
+        expiresIn: 3600
+      }
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     TypeOrmModule.forFeature([UserRepository])
   ],
   controllers: [AuthController],
